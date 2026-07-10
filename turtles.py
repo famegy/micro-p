ac0620403b1bd0c3d4da7609612eb52726dcc05c
Samuel Giks <samuelgiks254@gmail.com>
2026-07-10 18:42:36 +0300

    finished the practice

--- turtles.py
+++ turtles.py
@@ -23,6 +23,17 @@ def get_data():
             print("Please enter a number between 2 and 10.")
 
 
+def race_turtles(colors):
+    turtles = create_racer(colors)
+    while True:
+        for racer in turtles:
+            distance = random.randint(1, 20)
+            racer.forward(distance)
+            x, y = racer.pos()
+            if y >= HEIGHT // 2 - 20:
+                return colors[turtles.index(racer)]
+
+
 def create_racer(color):
     turtles = []
     spacingt = WIDTH // (len(color) + 1)
@@ -33,6 +44,7 @@ def create_racer(color):
         racer.left(90)
         racer.penup()
         racer.setpos(-WIDTH // 2 + (i + 1) * spacingt, -HEIGHT // 2 + 20)
+        racer.pendown()
         turtles.append(racer)
     return turtles
 
@@ -44,10 +56,22 @@ def create_turtles():
     # time.sleep(6)
 
 
+def draw_finish_line():
+    line = turtle.Turtle()
+    line.hideturtle()
+    line.penup()
+    line.speed(0)
+    line.setpos(-WIDTH // 2, HEIGHT // 2 - 20)
+    line.pendown()
+    line.setpos(WIDTH // 2, HEIGHT // 2 - 20)
+
+
 racers = get_data()
 data = create_turtles()
+draw_finish_line()
 colors = random.shuffle(COLORS)
 colors = COLORS[:racers]
-create_racer(colors)
-turtle.done()
+winner = race_turtles(colors)
+time.sleep(5)
 # print(colors)
+print(f"The {winner} turtle wins!")
